cbf5c2df10ec214af0e0377b92ae6c4780ea98cc
Justin Adams <justin@DESKTOP-GP5T8ES.localdomain>
2021-04-09 11:36:36 -0500

    added javascript

---
+++ scripts/database.js
@@ -0,0 +1,28 @@
+const database = {
+    fish: [
+        {
+            name: "Nemo",
+            species: "Clownfish",
+            food: "Plankton",
+            size: "4.3 inches",
+            location: "Red Sea",
+        },
+        {
+            name: "Snoop",
+            species: "Anglefish",
+            food: "Worms",
+            size: "6 inches",
+            location: "South America",
+        },
+        {
+            name: "Blob",
+            species: "Puffer Fish",
+            food: "Algae",
+            size: "5 inches",
+            location: "Pacific Ocean"
+        }
+]
+};
+
+console.log(database.fish)
+
